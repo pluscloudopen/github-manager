3070939032ecd88cb164b779d8939b1e29f943f1
Ralf Heiringhoff <ralf.heiringhoff@plusserver.com>
2023-02-28 01:30:35 +0100

    applied black + pylint suggestions
    Signed-off-by: Ralf Heiringhoff <ralf.heiringhoff@plusserver.com>

--- check_repositories.py
+++ check_repositories.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 import github
 import yaml
 
@@ -9,7 +10,7 @@ gh = github.Github(login_or_token=API_TOKEN)
 
 existing_repos = set()
 defined_repos = set()
-error = 0
+ERROR = 0
 
 for repo in gh.get_organization(ORGANIZATION).get_repos():
     existing_repos.add(repo.name)
@@ -17,21 +18,21 @@ for repo in gh.get_organization(ORGANIZATION).get_repos():
 repositories_dir = os.path.join("orgs/" + ORGANIZATION + "/repositories")
 
 for filename in os.listdir(repositories_dir):
-    if filename.endswith('.yaml') or filename.endswith('.yml'):
+    if filename.endswith(".yaml") or filename.endswith(".yml"):
         with open(os.path.join(repositories_dir, filename)) as f:
             yaml_data = yaml.safe_load(f)
             if yaml_data is not None:
-                defined_repos.add(list(yaml_data.keys())[0])   
+                defined_repos.add(list(yaml_data.keys())[0])
 
 not_on_github = defined_repos.difference(existing_repos)
 not_defined = existing_repos.difference(defined_repos)
 
-if (len(not_on_github) != 0):
+if len(not_on_github) != 0:
     print(f"Not on GitHub: {not_on_github}", file=sys.stderr)
-    error += 1
+    ERROR += 1
 
-if (len(not_defined) != 0):
+if len(not_defined) != 0:
     print(f"Not defined in github-manager: {not_defined}", file=sys.stderr)
-    error += 1
+    ERROR += 1
 
-sys.exit(error)
+sys.exit(ERROR)
